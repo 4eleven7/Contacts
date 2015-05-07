9982e39d4bb32b92e78c0dfc564d45941ca662bd
Dan Love <github@daniellove.net>
2015-05-07 15:07:05 +0100

    Added NSDate extension to find out where the date lies (weekend or before/after work)

---
+++ Contacts/Extensions/NSDate+LongAgo.swift
@@ -0,0 +1,26 @@
+//
+//  NSDate+LongAgo.swift
+//  Contacts
+//
+//  Created by Daniel Love on 07/05/2015.
+//  Copyright (c) 2015 Daniel Love. All rights reserved.
+//
+
+import Foundation
+
+extension NSDate
+{
+	func isWeekend() -> Bool
+	{
+		let calendar: NSCalendar = NSCalendar.currentCalendar()
+		return calendar.isDateInWeekend(self)
+	}
+	
+	func isBetweenTime(start:Int = 9, end: Int = 17) -> Bool
+	{
+		let calendar: NSCalendar = NSCalendar.currentCalendar()
+		let components  = calendar.components(NSCalendarUnit.CalendarUnitHour, fromDate: self)
+		
+		return components.hour > start && components.hour < end
+	}
+}
