28a5ca7270be2c4d458330af3d97818583a2964d
Dan Love <github@daniellove.net>
2015-05-07 14:46:33 +0100

    Check we have permission to access AddressBook

--- Contacts/Model/AddressBookHelper.swift
+++ Contacts/Model/AddressBookHelper.swift
@@ -12,5 +12,39 @@ import AddressBookUI
 
 class AddressBookHelper
 {
-	var addressBook: ABAddressBookRef?
+	var addressBook: ABAddressBookRef? =
+	{
+		var error: Unmanaged<CFErrorRef>?
+		var addressBook: ABAddressBookRef? = ABAddressBookCreateWithOptions(nil, &error)?.takeRetainedValue()
+		
+		if (error != nil) {
+			println("error occuring creating address book \(error)")
+		}
+		
+		return addressBook
+	}()
+	
+	func checkAuthorizationStatus() -> Bool
+	{
+		var error: Unmanaged<CFErrorRef>?
+		var authorised: Bool = false
+		
+		if addressBook != nil
+		{
+			let authStatus = ABAddressBookGetAuthorizationStatus()
+			if authStatus == ABAuthorizationStatus.NotDetermined || authStatus == ABAuthorizationStatus.Authorized
+			{
+				// Semaphore lock is used to wait until we get a response from the OS or user (via permission dialogue).
+				var lock = dispatch_semaphore_create(0)
+				ABAddressBookRequestAccessWithCompletion(addressBook,
+				{ (success, error) in
+					authorised = success && (error == nil)
+					dispatch_semaphore_signal(lock)
+				})
+				dispatch_semaphore_wait(lock, DISPATCH_TIME_FOREVER)
+			}
+		}
+		
+		return authorised
+	}
 }
