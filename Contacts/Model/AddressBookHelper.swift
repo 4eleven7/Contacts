5255554530897c19e8f2246148614b2a7d11c9fd
Dan Love <github@daniellove.net>
2015-05-07 14:44:56 +0100

    Created a wrapper class around AddressBook

---
+++ Contacts/Model/AddressBookHelper.swift
@@ -0,0 +1,16 @@
+//
+//  AddressBookHelper.swift
+//  Contacts
+//
+//  Created by Daniel Love on 07/05/2015.
+//  Copyright (c) 2015 Daniel Love. All rights reserved.
+//
+
+import Foundation
+import AddressBook
+import AddressBookUI
+
+class AddressBookHelper
+{
+	var addressBook: ABAddressBookRef?
+}
