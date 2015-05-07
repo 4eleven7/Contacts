82d68831c60a155ffb911e1eab53d87922ff78ad
Dan Love <github@daniellove.net>
2015-05-07 15:00:18 +0100

    Stubbed ContactsController, which will enable the filtering and sorting of Contact objects

---
+++ Contacts/Controller/ContactsController.swift
@@ -0,0 +1,23 @@
+//
+//  ContactsController.swift
+//  Contacts
+//
+//  Created by Daniel Love on 07/05/2015.
+//  Copyright (c) 2015 Daniel Love. All rights reserved.
+//
+
+import UIKit
+import AddressBook
+
+class ContactsController
+{
+	func updateContacts(contacts: [Contact])
+	{
+		
+	}
+	
+	func contactAtIndex(index: Int, includeNonImportant: Bool = true) -> Contact
+	{
+		return Contact()
+	}
+}
