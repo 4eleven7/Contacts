36d5fe196a53066ff0f42009f0745812424d2375
Dan Love <github@daniellove.net>
2015-05-11 15:11:28 +0100

    Added custom table cell for contacts

---
+++ Contacts/Views/ContactTableCell.swift
@@ -0,0 +1,17 @@
+//
+//  ContactTableCell.swift
+//  Contacts
+//
+//  Created by Daniel Love on 11/05/2015.
+//  Copyright (c) 2015 Daniel Love. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableCell: UITableViewCell
+{
+	@IBOutlet var contactNameLabel: UILabel?
+	@IBOutlet var dateTextLabel: UILabel?
+	@IBOutlet var contactMethodOne: UIButton?
+	@IBOutlet var contactMethodTwo: UIButton?
+}
